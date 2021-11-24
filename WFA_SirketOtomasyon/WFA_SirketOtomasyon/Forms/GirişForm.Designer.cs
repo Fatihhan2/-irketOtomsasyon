4a0461cf1f0c596c8b0b04bb89a939381c337bf1
fatihhan dalgıç <fatihhan_29@hotmail.com>
2021-11-24 14:06:25 +0300

    .

---
+++ WFA_SirketOtomasyon/WFA_SirketOtomasyon/Forms/GirişForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace WFA_SirketOtomasyon.Forms
+{
+    partial class GirişForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GirişForm));
+            this.txtkullanıcıadı = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtşifre = new System.Windows.Forms.TextBox();
+            this.btngiriş = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtkullanıcıadı
+            // 
+            this.txtkullanıcıadı.Location = new System.Drawing.Point(79, 9);
+            this.txtkullanıcıadı.Name = "txtkullanıcıadı";
+            this.txtkullanıcıadı.Size = new System.Drawing.Size(137, 20);
+            this.txtkullanıcıadı.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "KullanıcıAdı:";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.label2.Location = new System.Drawing.Point(34, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Şifre:";
+            // 
+            // txtşifre
+            // 
+            this.txtşifre.Location = new System.Drawing.Point(79, 35);
+            this.txtşifre.Name = "txtşifre";
+            this.txtşifre.Size = new System.Drawing.Size(137, 20);
+            this.txtşifre.TabIndex = 3;
+            // 
+            // btngiriş
+            // 
+            this.btngiriş.Location = new System.Drawing.Point(79, 62);
+            this.btngiriş.Name = "btngiriş";
+            this.btngiriş.Size = new System.Drawing.Size(137, 23);
+            this.btngiriş.TabIndex = 4;
+            this.btngiriş.Text = "GirişYap";
+            this.btngiriş.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Location = new System.Drawing.Point(261, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(189, 198);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // GirişForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(452, 206);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btngiriş);
+            this.Controls.Add(this.txtşifre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtkullanıcıadı);
+            this.Name = "GirişForm";
+            this.Text = "GirişForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtkullanıcıadı;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtşifre;
+        private System.Windows.Forms.Button btngiriş;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
